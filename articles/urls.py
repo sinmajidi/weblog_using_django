20d0bec9421c4df89a13c1d26c2c0aae438aa2b2
sina majidi <sin.majidi@gmail.com>
2021-10-21 14:44:15 +0330

    if you click on articles title,the articles slug returned

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 from . import views
-
+app_name='articles'
 urlpatterns = [
 
     path('', views.articles_list),
+    path('<slug>', views.articles_detail,name="detail"),
 
 
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from django.shortcuts import HttpResponse
 from . import models
 def articles_list (request):
     articles=models.Articles.objects.all().order_by('date')
     args={'articles':articles}
     return render(request,'articles/articleslist.html',args)
+
+def articles_detail (request,slug):
+
+    return HttpResponse(slug)
 
 
