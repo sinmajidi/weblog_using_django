0a328970d2ab285d43d608180fdd4143f83cc4c7
sina majidi <sin.majidi@gmail.com>
2021-10-23 14:25:29 +0330

    pluse logout button to project

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout
 def signup_view (request):
     if request.method=='POST':
         form=UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user=form.save()
+            login(request, user)
             return redirect('articles:list')
     else:
 
@@ -16,11 +18,17 @@ def login_view (request):
     if request.method=='POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # login
+            user=form.get_user()
+            login(request,user)
             return redirect('articles:list')
     else:
 
         form=AuthenticationForm()
         # args={'form':form}
     return render(request,'accounts/login.html',{'form':form})
+
+def logout_view (request):
+    if request.method=='POST':
+        logout(request)
+    return redirect('articles:list')
 
