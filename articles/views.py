aa41256ce30c074af2e9ed0abb206994861fe288
sina majidi <sin.majidi@gmail.com>
2021-10-23 14:54:35 +0330

    you should login to access to create articles page,try domain/articles/create

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,HttpResponse
-from django.shortcuts import HttpResponse
+from django.contrib.auth.decorators import login_required
 from . import models
 def articles_list (request):
     articles=models.Articles.objects.all().order_by('date')
@@ -11,3 +11,6 @@ def articles_detail (request,slug):
     # return HttpResponse(slug)
     article=models.Articles.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article':article})
+@login_required(login_url="/accounts/login")
+def create_article (request):
+    return render(request, 'articles/create_article.html')
