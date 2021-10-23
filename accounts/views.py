4fd8461d65cf4bcdaaff79d2293102bd0974eb75
sina majidi <sin.majidi@gmail.com>
2021-10-23 12:06:24 +0330

    complete the signup form and validation for registering users on domain/account/signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 def signup_view (request):
-    form=UserCreationForm()
-    args={'form':form}
-    return render(request,'accounts/signup.html',args)
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #login
+            return redirect('articles:list')
+    else:
+
+        form=UserCreationForm()
+        # args={'form':form}
+    return render(request,'accounts/signup.html',{'form':form})
 
--- articles/urls.py
+++ articles/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name='articles'
 urlpatterns = [
 
-    path('', views.articles_list),
+    path('', views.articles_list,name="list"),
     path('<slug>', views.articles_detail,name="detail"),
 
 
