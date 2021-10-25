666837537202f8da50a7f23fccd71a220831cc56
sina majidi <sin.majidi@gmail.com>
2021-10-26 00:07:37 +0330

    i add article creation form

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from . import models
+from . import create_form
 def articles_list (request):
     articles=models.Articles.objects.all().order_by('date')
     args={'articles':articles}
@@ -13,4 +14,10 @@ def articles_detail (request,slug):
     return render(request, 'articles/article_detail.html', {'article':article})
 @login_required(login_url="/accounts/login")
 def create_article (request):
-    return render(request, 'articles/create_article.html')
+    if request.method=='POST':
+        form=create_form.CreateArticle(request.POST,request.FILES)
+        if form.is_valid:
+            return redirect('articles:list')
+    else:
+        form=create_form.CreateArticle()
+    return render(request, 'articles/create_article.html',{'form':form})
